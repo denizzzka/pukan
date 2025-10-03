a93109765a233429280e4d9acb0ba0c3663c052c
CRy386i <tetris123480@gmail.com>
2025-10-04 01:12:16 +0200

    deleted a garbage stuff

--- source/pukan/vulkan/bindings.c
+++ source/pukan/vulkan/bindings.c
@@ -1,16 +1,3 @@
 // This trampoline file is need because importC can't import .h files directly
 
 #include <vulkan/vulkan.h>
-
-/* It is not necessary, vulkan.h doeas the same perfeclty */
-#if 0
-#ifdef _WIN32
-    #include <windows.h> // It's important thing for Windows!
-    #include <vulkan/vulkan_win32.h>
-#else
-    // Posix
-    #include <xcb/xcb.h>
-    #include <vulkan/vulkan_xcb.h>
-    #include <vulkan/vulkan_wayland.h>
-#endif
-#endif
