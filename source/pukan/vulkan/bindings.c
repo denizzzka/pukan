d0f1ae100a0af65ad5f508a39e3775a5807d815f
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-04 18:28:05 +0300

    Windows support fix (by CRy386i)

--- source/pukan/vulkan/bindings.c
+++ source/pukan/vulkan/bindings.c
@@ -1,12 +1,3 @@
 // This trampoline file is need because importC can't import .h files directly
 
 #include <vulkan/vulkan.h>
-
-#ifdef WIN32
-    #include <vulkan/vulkan_win32.h>
-#else
-    // Posix
-    #include <xcb/xcb.h>
-    #include <vulkan/vulkan_xcb.h>
-    #include <vulkan/vulkan_wayland.h>
-#endif
