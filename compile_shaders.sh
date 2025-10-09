7cff8f4274a24899c6393e20951acb7050b5da71
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-09 15:38:09 +0300

    Fragment shader split into textured and colored

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -3,3 +3,4 @@
 glslc -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o vert.spv
 
 glslc -fshader-stage=frag source/pukan/shaders/fragment.glsl -o frag.spv
+glslc -fshader-stage=frag source/pukan/shaders/textured_fragment.glsl -o textured_frag.spv
--- source/pukan/shaders/fragment.glsl
+++ source/pukan/shaders/fragment.glsl
@@ -1,13 +1,8 @@
 #version 450
 
-layout(binding = 1) uniform sampler2D textureSampler;
-
 layout(location = 0) in vec3 fragColor;
-layout(location = 1) in vec2 fragTextureCoord;
-
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    //~ outColor = vec4(fragColor, 1.0);
-    outColor = texture(textureSampler, fragTextureCoord);
+    outColor = vec4(fragColor, 1.0);
 }
