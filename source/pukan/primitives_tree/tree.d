297e543776f64b67025d190c5c527e51347500f7
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-10 11:16:49 +0300

    Traversal drawingBufferFilling

--- source/pukan/primitives_tree/tree.d
+++ source/pukan/primitives_tree/tree.d
@@ -32,21 +32,34 @@ class PrimitivesTree //TODO: DrawableByVulkan
 
     void forEachNode(void delegate(ref Node) dg) => root.traversal(dg);
 
+    import dlib.math;
+
     void drawingBufferFilling(VkCommandBuffer buf, VkDescriptorSet[] descriptorSets)
     {
-        auto dr = root.payload.peek!Drawable;
-        auto pcfg = pipelinesConfig[dr.pipelineCfgIdx];
+        drawingBufferFilling(buf, descriptorSets, root, Matrix4f.identity);
+    }
 
-        //FIXME: remove and use bone value
-        import dlib.math: Matrix4f;
-        auto noTrans = Matrix4f.identity;
+    private void drawingBufferFilling(VkCommandBuffer buf, VkDescriptorSet[] descriptorSets, ref Node curr, Matrix4x4f trans)
+    {
+        if(curr.payload.type == typeid(Bone))
+        {
+            trans *= curr.payload.peek!Bone.mat;
+        }
+        else if(curr.payload.type == typeid(Drawable))
+        {
+            auto dr = root.payload.peek!Drawable;
+            auto pcfg = pipelinesConfig[dr.pipelineCfgIdx];
 
-        dr.drawingBufferFilling(
-            buf,
-            pcfg.graphicsPipeline,
-            pcfg.pipelineLayout,
-            descriptorSets,
-            noTrans,
-        );
+            dr.drawingBufferFilling(
+                buf,
+                pcfg.graphicsPipeline,
+                pcfg.pipelineLayout,
+                descriptorSets,
+                trans,
+            );
+        }
+
+        foreach(ref c; curr.children)
+            drawingBufferFilling(buf, descriptorSets, c, trans);
     }
 }
