88121a4861bef5790d942ea5931ec4f0e43e404b
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-12 17:37:43 +0300

    glTF file read, version check

--- source/pukan/gltf/package.d
+++ source/pukan/gltf/package.d
@@ -1,12 +1,25 @@
 module pukan.gltf;
 
+import std.exception: enforce;
 static import std.file;
+static import std.path;
+import pukan.exceptions;
 import vibe.data.json;
 
 ///
 auto loadGlTF2(string filename)
 {
-    const txt = std.file.readText(filename);
+    const json = std.file.readText(filename).parseJsonString;
+    const dir = std.path.dirName(filename);
+
+    import std.stdio;
+    writeln(filename);
+    writeln(dir);
+
+    {
+        const ver = json["asset"]["version"].get!string;
+        enforce!PukanException(ver == "2.0", "glTF version "~ver~" unsupported");
+    }
 
     return 0;
 }
