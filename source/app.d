2dfab6177ef3513f5392f84f6dcc8bb0d7caffde
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-17 00:12:24 +0300

    Texture ctor wants VkSamplerCreateInfo arg

--- source/app.d
+++ source/app.d
@@ -295,7 +295,25 @@ auto createDemoTree(LogicalDevice device, Scene scene, FrameBuilder frameBuilder
 
     {
         auto img = loadImageFromFile("demo/assets/texture.jpeg");
-        auto texture = device.create!Texture(frameBuilder.commandPool, commandBuffer, &img);
+
+        VkSamplerCreateInfo samplerInfo;
+        {
+            samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+            samplerInfo.magFilter = VK_FILTER_LINEAR;
+            samplerInfo.minFilter = VK_FILTER_LINEAR;
+            samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+            samplerInfo.anisotropyEnable = VK_TRUE;
+            samplerInfo.maxAnisotropy = 16; //TODO: use vkGetPhysicalDeviceProperties (at least)
+            samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+            samplerInfo.unnormalizedCoordinates = VK_FALSE;
+            samplerInfo.compareEnable = VK_FALSE;
+            samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+            samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        }
+
+        auto texture = device.create!Texture(frameBuilder.commandPool, commandBuffer, samplerInfo, &img);
         auto mesh = scene.texturedMeshFactory.create(scene.frameBuilder, texturedVertices, texturedIndices, texture);
 
         textureBranch.addChildNode(mesh);
--- source/pukan/vulkan/textures.d
+++ source/pukan/vulkan/textures.d
@@ -14,7 +14,7 @@ class Texture
     VkImageView imageView;
     VkSampler sampler;
 
-    this(Img)(LogicalDevice device, CommandPool commandPool, VkCommandBuffer commandBuf, Img image)
+    this(Img)(LogicalDevice device, CommandPool commandPool, VkCommandBuffer commandBuf, ref VkSamplerCreateInfo samplerInfo, Img image)
     {
         this.device = device;
 
@@ -22,24 +22,7 @@ class Texture
 
         createImageView(imageView, device, VK_FORMAT_R8G8B8A8_SRGB, textureImageMemory.image);
 
-        VkSamplerCreateInfo samplerInfo;
-        {
-            samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-            samplerInfo.magFilter = VK_FILTER_LINEAR;
-            samplerInfo.minFilter = VK_FILTER_LINEAR;
-            samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.anisotropyEnable = VK_TRUE;
-            samplerInfo.maxAnisotropy = 16; //TODO: use vkGetPhysicalDeviceProperties (at least)
-            samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-            samplerInfo.unnormalizedCoordinates = VK_FALSE;
-            samplerInfo.compareEnable = VK_FALSE;
-            samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-            samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-        }
-
-        vkCall(device.device, &samplerInfo, device.alloc, &sampler);
+        vkCall(device.device, cast() &samplerInfo, device.alloc, &sampler);
     }
 
     ~this()
