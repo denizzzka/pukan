851d959706fd644d4811157cf1dbe99232ffbd42
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-20 15:13:26 +0300

    PrimitivesFactory moved to dedicated module

---
+++ source/pukan/primitives_tree/factory.d
@@ -0,0 +1,40 @@
+module pukan.primitives_tree.factory;
+
+import pukan.scene: Vertex;
+
+struct PrimitivesFactory(T)
+{
+    import pukan.vulkan;
+    import shaders = pukan.vulkan.shaders;
+    import pukan.vulkan.frame_builder;
+
+    LogicalDevice device;
+    private PoolAndLayoutInfo poolAndLayout;
+    private DefaultGraphicsPipelineInfoCreator!Vertex pipelineInfoCreator;
+    GraphicsPipelineCfg graphicsPipelineCfg;
+
+    this(LogicalDevice device, ShaderInfo[] shaderStages, RenderPass renderPass)
+    {
+        this.device = device;
+
+        auto layoutBindings = shaders.createLayoutBinding(shaderStages);
+        poolAndLayout = device.createDescriptorPool(layoutBindings, 10 /*FIXME*/);
+
+        pipelineInfoCreator = new DefaultGraphicsPipelineInfoCreator!Vertex(device, [poolAndLayout.descriptorSetLayout], shaderStages, renderPass);
+        graphicsPipelineCfg.pipelineLayout = pipelineInfoCreator.pipelineLayout;
+
+        auto pipelineCreateInfo = pipelineInfoCreator.pipelineCreateInfo;
+        graphicsPipelineCfg.graphicsPipeline = device.createGraphicsPipelines([pipelineCreateInfo])[0];
+    }
+
+    auto create(CTOR_ARGS...)(FrameBuilder frameBuilder, CTOR_ARGS args)
+    {
+        assert(device);
+        auto descriptorsSet = device.allocateDescriptorSets(poolAndLayout, 1);
+
+        auto r = new T(descriptorsSet, args);
+        r.updateDescriptorSets(device);
+
+        return r;
+    }
+}
--- source/pukan/primitives_tree/package.d
+++ source/pukan/primitives_tree/package.d
@@ -2,6 +2,7 @@ module pukan.primitives_tree;
 
 import pukan.primitives_tree.mesh;
 import pukan.primitives_tree.tree: PrimitivesTree;
+public import pukan.primitives_tree.factory: PrimitivesFactory;
 import pukan.scene: Vertex;
 import pukan.vulkan.bindings;
 import pukan.vulkan.pipelines: GraphicsPipelineCfg;
@@ -66,41 +67,4 @@ struct Bone
     alias this = mat;
 
     uint translationBufferIdx;
-}
-
-struct PrimitivesFactory(T)
-{
-    import pukan.vulkan;
-    import shaders = pukan.vulkan.shaders;
-    import pukan.vulkan.frame_builder;
-
-    LogicalDevice device;
-    private PoolAndLayoutInfo poolAndLayout;
-    private DefaultGraphicsPipelineInfoCreator!Vertex pipelineInfoCreator;
-    GraphicsPipelineCfg graphicsPipelineCfg;
-
-    this(LogicalDevice device, ShaderInfo[] shaderStages, RenderPass renderPass)
-    {
-        this.device = device;
-
-        auto layoutBindings = shaders.createLayoutBinding(shaderStages);
-        poolAndLayout = device.createDescriptorPool(layoutBindings, 10 /*FIXME*/);
-
-        pipelineInfoCreator = new DefaultGraphicsPipelineInfoCreator!Vertex(device, [poolAndLayout.descriptorSetLayout], shaderStages, renderPass);
-        graphicsPipelineCfg.pipelineLayout = pipelineInfoCreator.pipelineLayout;
-
-        auto pipelineCreateInfo = pipelineInfoCreator.pipelineCreateInfo;
-        graphicsPipelineCfg.graphicsPipeline = device.createGraphicsPipelines([pipelineCreateInfo])[0];
-    }
-
-    auto create(CTOR_ARGS...)(FrameBuilder frameBuilder, CTOR_ARGS args)
-    {
-        assert(device);
-        auto descriptorsSet = device.allocateDescriptorSets(poolAndLayout, 1);
-
-        auto r = new T(descriptorsSet, args);
-        r.updateDescriptorSets(device);
-
-        return r;
-    }
 }
