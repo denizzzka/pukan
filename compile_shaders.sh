b17297ffce3239fd86bd1ca763f7812b35469835
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-22 00:54:47 +0300

    shaders compilation: vulkan version added to script

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-glslc -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o vert.spv
-glslc -fshader-stage=vertex source/pukan/shaders/gltf_vertices.glsl -o gltf_vertices.spv
+TGT="--target-env=vulkan1.2"
 
-glslc -fshader-stage=frag source/pukan/shaders/colored_fragment.glsl -o colored_frag.spv
-glslc -fshader-stage=frag source/pukan/shaders/textured_fragment.glsl -o textured_frag.spv
-glslc -fshader-stage=frag source/pukan/shaders/gltf_fragment.glsl -o gltf_fragment.spv
+glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o vert.spv
+glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/gltf_vertices.glsl -o gltf_vertices.spv
+
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/colored_fragment.glsl -o colored_frag.spv
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/textured_fragment.glsl -o textured_frag.spv
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/gltf_fragment.glsl -o gltf_fragment.spv
