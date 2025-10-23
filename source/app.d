7b4205631b80f0912e5640321894b716e37a1807
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-23 14:43:31 +0300

    gltf: SimpleTexture added to objects demo, one triangle rendered wrong

--- source/app.d
+++ source/app.d
@@ -297,6 +297,15 @@ auto createDemoTree(LogicalDevice device, Scene scene, FrameBuilder frameBuilder
     }
 
     {
+        auto trans = Vector3f(0.2, 0.2, 0.2).scaleMatrix * Vector3f(1, 1, 2.3).translationMatrix;
+
+        auto gltfObj = scene.gltfFactory.create("demo/assets/gltf_samples/SimpleTexture/glTF/SimpleTexture.gltf");
+        tree.root
+            .addChild(Bone(mat: trans))
+            .addChild(gltfObj);
+    }
+
+    {
         auto trans = Vector3f(0.2, 0.2, 0.2).scaleMatrix * Vector3f(-1, 1, 0.3).translationMatrix;
 
         auto gltfObj = scene.gltfFactory.create("demo/assets/gltf_samples/AnimatedCube/glTF/AnimatedCube.gltf");
