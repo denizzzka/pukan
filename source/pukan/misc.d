07d959311dd9af30d5e0b843fae36c717123f6d1
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-25 20:51:25 +0300

    loadImageFromMemory added

--- source/pukan/misc.d
+++ source/pukan/misc.d
@@ -1,21 +1,33 @@
 module pukan.misc;
 
+import gamut;
 import pukan.exceptions;
 import std.conv: to;
 import std.exception: enforce;
 
-auto loadImageFromFile(string filepath)
+Image loadImageFromFile(string filepath)
 {
-    import gamut;
-
     Image image;
     image.loadFromFile(filepath, LAYOUT_GAPLESS|LAYOUT_VERT_STRAIGHT|LOAD_ALPHA);
 
+    check(image);
+    return image;
+}
+
+Image loadImageFromMemory(const(ubyte)[] blob)
+{
+    Image image;
+    image.loadFromMemory(blob);
+
+    check(image);
+    return image;
+}
+
+private void check(ref Image image)
+{
     if(image.isError)
         throw new PukanException(image.errorMessage.to!string);
 
     enforce!PukanException(image.type == PixelType.rgba8, "Unsupported texture type: "~image.type.to!string);
     enforce!PukanException(image.layers == 1, "Texture image must contain one layer");
-
-    return image;
 }
