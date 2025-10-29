4ca8d72e3a035240a5fe21c43eb22894521bade1
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-29 17:13:15 +0300

    drawingBufferFilling: comment added

--- source/pukan/gltf/package.d
+++ source/pukan/gltf/package.d
@@ -284,6 +284,7 @@ class GlTF : DrawableByVulkan
 
     void drawingBufferFilling(VkCommandBuffer buf, Matrix4x4f trans)
     {
+        // To avoid mirroring if loaded OpenGL mesh into Vulkan
         trans *= Vector3f(-1, -1, -1).scaleMatrix;
 
         vkCmdBindPipeline(buf, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.graphicsPipeline);
