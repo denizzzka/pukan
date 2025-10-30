874e6d5e602f2ffb388fa030162fcdea187222af
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-30 19:35:51 +0300

    *.spv moved to compiled_shaders/

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -2,9 +2,9 @@
 
 TGT="--target-env=vulkan1.2"
 
-glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o vert.spv
-glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/gltf_vertices.glsl -o gltf_vertices.spv
+glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o compiled_shaders/vert.spv
+glslc ${TGT} -fshader-stage=vertex source/pukan/shaders/gltf_vertices.glsl -o compiled_shaders/gltf_vertices.spv
 
-glslc ${TGT} -fshader-stage=frag source/pukan/shaders/colored_fragment.glsl -o colored_frag.spv
-glslc ${TGT} -fshader-stage=frag source/pukan/shaders/textured_fragment.glsl -o textured_frag.spv
-glslc ${TGT} -fshader-stage=frag source/pukan/shaders/gltf_fragment.glsl -o gltf_fragment.spv
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/colored_fragment.glsl -o compiled_shaders/colored_frag.spv
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/textured_fragment.glsl -o compiled_shaders/textured_frag.spv
+glslc ${TGT} -fshader-stage=frag source/pukan/shaders/gltf_fragment.glsl -o compiled_shaders/gltf_fragment.spv
