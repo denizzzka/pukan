6cebd45d89bd971e0576a0b20e18d989547b9af3
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-10-31 19:11:55 +0300

    BufAccess.bindVertexBuffer() added

--- source/pukan/gltf/loader.d
+++ source/pukan/gltf/loader.d
@@ -464,6 +464,19 @@ struct BufAccess
     uint offset;
     ubyte stride;
     uint count;
+
+    //TODO: bind whole bunch of object buffers?
+    void bindVertexBuffer(BufferPieceOnGPU gpuBuf, VkCommandBuffer cmdBuf)
+    in(viewIdx >= 0)
+    in(count)
+    {
+        VkBuffer[1] buffers = [gpuBuf.buffer.gpuBuffer.buf.getVal()];
+        immutable VkDeviceSize[1] offsets = [offset];
+        immutable VkDeviceSize[1] sizes = [count];
+        immutable VkDeviceSize[1] strides = [stride];
+
+        vkCmdBindVertexBuffers2(cmdBuf, 0, 1, buffers.ptr, offsets.ptr, sizes.ptr, strides.ptr);
+    }
 }
 
 private struct AccessRange(T)
