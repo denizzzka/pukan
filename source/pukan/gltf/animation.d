11e3d86b06dcd88fd9fa52757bffb4cae46376e6
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-11-04 20:43:36 +0300

    getSampleByTime stub added, AnimationSupport ctor added

--- source/pukan/gltf/animation.d
+++ source/pukan/gltf/animation.d
@@ -31,6 +31,24 @@ struct AnimationSampler
 
     /// Accessor for keyframe values
     BufAccess output;
+
+    /**
+     * Finds the keyframe sample indices and times for a given animation time.
+     *
+     * Params:
+     *   t            = Current animation time.
+     *   previousTime = Output: previous keyframe time.
+     *   nextTime     = Output: next keyframe time.
+     *   loopTime     = Output: wrapped time within the animation duration.
+     * Returns:
+     *   Index of the previous keyframe.
+     */
+    size_t getSampleByTime(in float t, out float previousTime, out float nextTime, out float loopTime)
+    {
+        assert(input.viewIdx >= 0);
+
+        return 0; // No translation found, so using first translation
+    }
 }
 
 /// Represents a GLTF animation channel, which targets a node and property (TRS)
@@ -58,7 +76,17 @@ struct Animation
 
 package struct AnimationSupport
 {
+    import pukan.gltf.loader;
+
+    private GltfContent* content;
+
     import dlib.math;
 
     Matrix4x4f[] perNodeTranslations;
+
+    package this(GltfContent* c, size_t nodesNum)
+    {
+        content = c;
+        perNodeTranslations.length = nodesNum;
+    }
 }
